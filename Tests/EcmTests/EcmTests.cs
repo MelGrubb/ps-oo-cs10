d44556d3ce15d920b8cf21ef9b493ac35f6db871
Mel Grubb <MelGrubb@live.com>
2022-12-11 09:51:42 -0800

    05-solid-isp-start

---
+++ Tests/EcmTests/EcmTests.cs
@@ -0,0 +1,35 @@
+﻿using Demo;
+using NUnit.Framework;
+
+namespace Tests.EcmTests
+{
+    [TestFixture]
+    public class EcmTests
+    {
+        private void TestEcm(EngineControlModule sut)
+        {
+            sut.Start();
+            Assert.IsTrue(sut.IsRunning);
+            sut.Stop();
+            Assert.IsFalse(sut.IsRunning);
+        }
+
+        [Test]
+        public void TestIceControlModule()
+        {
+            TestEcm(new IceControlModule());
+        }
+
+        [Test]
+        public void TestElectricControlModule()
+        {
+            TestEcm(new ElectricControlModule());
+        }
+
+        [Test]
+        public void TestHybridControlModule()
+        {
+            TestEcm(new HybridControlModule());
+        }
+    }
+}
