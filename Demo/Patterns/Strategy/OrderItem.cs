fc7cd6d92e2b8e06341c3a7bd60b4a62065de616
Mel Grubb <MelGrubb@live.com>
2022-12-28 18:14:49 -0800

    Strategy prep

--- Demo/Patterns/Strategy/OrderItem.cs
+++ Demo/Patterns/Strategy/OrderItem.cs
@@ -3,7 +3,7 @@
     public class OrderItem
     {
         public int Id { get; set; }
-        public int Quantity { get;set }
+        public int Quantity { get; set; }
         public decimal Price { get; set; }
     }
 }
