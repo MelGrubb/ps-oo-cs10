9ebafa65868dc54e5f8c6a88c11bc1aa7e24fb30
Mel Grubb <MelGrubb@live.com>
2023-01-07 10:20:23 -0800

    06-nullable-consuming

--- Demo/Nullability/Person.cs
+++ Demo/Nullability/Person.cs
@@ -1,4 +1,6 @@
-﻿#nullable disable
+﻿#nullable enable
+
+using System;
 
 namespace Demo.Nullability
 {
@@ -6,10 +8,17 @@ namespace Demo.Nullability
     {
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
-        public string MiddleName { get; set; }
+        public string? MiddleName { get; set; } = null;
         public string LastName { get; set; }
-        public float Height { get; set; } = null;
-        public CreditCard CreditCard { get; set; } = null;
+        public float? Height { get; set; } = null;
+        public CreditCard? CreditCard { get; set; } = null;
+
+        public Person(string emailAddress, string firstName, string lastName)
+        {
+            EmailAddress = emailAddress;
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 
     public class CreditCard
