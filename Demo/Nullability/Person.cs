3fea2804a61c7a679ef2d7718862899162fe4949
Mel Grubb <MelGrubb@live.com>
2023-01-07 08:35:30 -0800

    06-nullable-start

---
+++ Demo/Nullability/Person.cs
@@ -0,0 +1,31 @@
+﻿#nullable disable
+
+namespace Demo.Nullability
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public float Height { get; set; }
+        public CreditCard CreditCard { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    public class CreditCard
+    {
+        public int Id { get; set; }
+        public string CardNumber { get; set; }
+
+        public CreditCard(string cardNumber)
+        {
+            CardNumber = cardNumber;
+        }
+    }
+}
