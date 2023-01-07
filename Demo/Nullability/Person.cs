136bd189bd45088083c73717c6f9f5ebec7f708c
Mel Grubb <MelGrubb@live.com>
2023-01-07 11:02:53 -0800

    06-nullable-checking-done

--- Demo/Nullability/Person.cs
+++ Demo/Nullability/Person.cs
@@ -6,10 +6,31 @@ namespace Demo.Nullability
 {
     public class Person
     {
-        public string EmailAddress { get; set; }
-        public string FirstName { get; set; }
+        private string _emailAddress = null!;
+        private string _firstName;
+        private string _lastName;
+
+        /// <summary>Gets or sets the <see cref="Person"/>'s email address.</summary>
+        /// <exception cref="ArgumentNullException">Thrown if the property is set to a null value.</exception>
+        public string EmailAddress
+        {
+            get => _emailAddress;
+            set => _emailAddress = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
         public string? MiddleName { get; set; } = null;
-        public string LastName { get; set; }
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
         public float? Height { get; set; } = null;
         public CreditCard? CreditCard { get; set; } = null;
 
