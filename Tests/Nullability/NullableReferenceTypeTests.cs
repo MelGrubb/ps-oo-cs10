a396339d6d3a2ed1afe70b6041465c0abad08554
Mel Grubb <MelGrubb@live.com>
2023-01-07 10:34:09 -0800

    06-nullable-consuming

---
+++ Tests/Nullability/NullableReferenceTypeTests.cs
@@ -0,0 +1,23 @@
+﻿using Demo.Nullability;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable disable
+
+namespace Tests.Nullability
+{
+    [TestFixture]
+    public class NullableReferenceTypeTests
+    {
+        [Test]
+        public void Setting_EmailAddress_to_null()
+        {
+            var person = new Person(null, "John", "Doe");
+            Assert.IsNull(person.EmailAddress);
+        }
+    }
+}
