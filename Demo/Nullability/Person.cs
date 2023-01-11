0ae980b8ce98aec11ec6bed3f6a4147d73b305c1
Mel Grubb <MelGrubb@live.com>
2023-01-10 19:46:44 -0800

    Revert "06-nullable-settings"
    This reverts commit 7062b1f946f9e928724284adf06c02790b73bd0e.

--- Demo/Nullability/Person.cs
+++ Demo/Nullability/Person.cs
@@ -1,27 +1,12 @@
-﻿using System;
-
-#nullable enable
+﻿#nullable enable
 
 namespace Demo.Nullability
 {
     public class Person
     {
-        private string emailAddress = null!;
-        private string firstName = null!;
-        private string lastName = null!;
-
-        /// <summary>Gets or sets the <see cref="Person"/>'s email address.</summary>
-        /// <exception cref="ArgumentNullException">Thrown if the property is set to a null value.</exception>
-        public string EmailAddress { get => emailAddress; set => emailAddress = value; }
-
-        /// <summary>Gets or sets the <see cref="Person"/>'s first name.</summary>
-        /// <exception cref="ArgumentNullException">Thrown if the property is set to a null value.</exception>
-        public string FirstName { get => firstName; set => firstName = value; }
-
-        /// <summary>Gets or sets the <see cref="Person"/>'s last name.</summary>
-        /// <exception cref="ArgumentNullException">Thrown if the property is set to a null value.</exception>
-        public string LastName { get => lastName; set => lastName = value; }
-
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string? MiddleName { get; set; } = null;
         public float? Height { get; set; } = null;
         public CreditCard? CreditCard { get; set; } = null;
