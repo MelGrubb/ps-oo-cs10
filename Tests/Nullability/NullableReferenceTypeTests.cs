7062b1f946f9e928724284adf06c02790b73bd0e
Mel Grubb <MelGrubb@live.com>
2023-01-10 19:44:39 -0800

    06-nullable-settings

---
+++ Tests/Nullability/NullableReferenceTypeTests.cs
@@ -0,0 +1,21 @@
+﻿using Demo.Nullability;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Nullability
+{
+    [TestFixture]
+    public class NullableReferenceTypeTests
+    {
+        [Test]
+        public void Setting_EmailAddress_to_null()
+        {
+            var person = new Person(null, "John", "Doe");
+            Assert.IsNull(person.EmailAddress);
+        }
+    }
+}
