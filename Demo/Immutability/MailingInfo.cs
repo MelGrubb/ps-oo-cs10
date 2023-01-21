dd44153fc8786af7de2368be5b7d48bea1d1127f
Mel Grubb <MelGrubb@live.com>
2023-01-21 13:58:00 -0800

    07-records-iequatable-end

--- Demo/Immutability/MailingInfo.cs
+++ Demo/Immutability/MailingInfo.cs
@@ -2,7 +2,7 @@
 
 namespace Demo.Immutability
 {
-    public class MailingInfo
+    public sealed class MailingInfo : IEquatable<MailingInfo>
     {
         public string FullName { get; init; }
         public string Address1 { get; init; }
@@ -27,6 +27,49 @@ namespace Demo.Immutability
             Region = region;
             PostalCode = postalCode;
             Country = country;
+        }
+
+        public bool Equals(MailingInfo? other)
+        {
+            if (other == null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (other.GetType() != GetType()) return false;
+
+            return FullName == other.FullName
+                && Address1 == other.Address1
+                && City == other.City
+                && Region == other.Region
+                && PostalCode == other.PostalCode
+                && Country == other.Country;
+        }
+
+        public static bool operator ==(MailingInfo? left, MailingInfo? right)
+        {
+            if (left is null) { return right is null; }
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(MailingInfo? left, MailingInfo? right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object? other)
+        {
+            return Equals(other as MailingInfo);
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = new HashCode();
+            hash.Add(FullName);
+            hash.Add(Address1);
+            hash.Add(City);
+            hash.Add(Region);
+            hash.Add(PostalCode);
+            hash.Add(Country);
+
+            return hash.ToHashCode();
         }
     }
 }
--- Tests/EqualityTests/MailingInfoEqualityTests.cs
+++ Tests/EqualityTests/MailingInfoEqualityTests.cs
@@ -9,11 +9,17 @@ namespace Tests.EqualityTests
         [Test]
         public void MailingInfo_equality()
         {
-            var info1 = new MailingInfo("John Doe", "123 Any St.", "Anytown", "OH", "43210", "USA");
-            var info2 = new MailingInfo("John Doe", "123 Any St.", "Anytown", "OH", "43210", "USA");
+            var info1 = new MailingInfo("John Doe", "123 Any St.", "Anytown", "OH", "43210", "USA")
+            { Address2 = "Suite 456" };
+            
+            var info2 = new MailingInfo("John Doe", "123 Any St.", "Anytown", "OH", "43210", "USA")
+            { Address2 = "Apt A" };
 
-            Assert.AreNotEqual(info1, info2);
+            Assert.AreEqual(info1, info2);
+            Assert.AreEqual((object)info1, (object)info2);
+            Assert.IsTrue(info1 == info2);
             Assert.AreNotSame(info1, info2);
+            Assert.AreEqual(info1.GetHashCode(), info2.GetHashCode());
         }
     }
 }
