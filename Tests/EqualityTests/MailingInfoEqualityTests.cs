cee44706d60b3c42898681932f42db8344ca9950
Mel Grubb <MelGrubb@live.com>
2023-01-22 18:18:04 -0800

    070401-start

--- Tests/EqualityTests/MailingInfoEqualityTests.cs
+++ Tests/EqualityTests/MailingInfoEqualityTests.cs
@@ -9,15 +9,12 @@ namespace Tests.EqualityTests
         [Test]
         public void MailingInfo_equality()
         {
-            var info1 = new MailingInfo("John Doe", "123 Any St.", "Anytown", "OH", "43210", "USA")
-            { Address2 = "Suite 456" };
-            var info2 = new MailingInfo("John Doe", "123 Any St.", "Anytown", "OH", "43210", "USA")
-            { Address2 = "Apt A" };
+            var info1 = new MailingInfo("John Doe", "123 Any St.", "Suite 456", "Anytown", "OH", "43210", "USA");
+            var info2 = new MailingInfo("John Doe", "123 Any St.", "Suite 456", "Anytown", "OH", "43210", "USA");
 
             Assert.AreEqual(info1, info2);
             Assert.AreEqual((object)info1, (object)info2);
             Assert.IsTrue(info1 == info2);
-            Assert.AreNotSame(info1, info2);
             Assert.AreEqual(info1.GetHashCode(), info2.GetHashCode());
         }
     }
