f019365674067162773722befce763ffa656f0d0
Mel Grubb <MelGrubb@live.com>
2023-01-29 19:08:20 -0800

    070503-start

--- Demo/Immutability/MailingInfo.cs
+++ Demo/Immutability/MailingInfo.cs
@@ -1,4 +1,4 @@
 ﻿namespace Demo.Immutability
 {
-    public record MailingInfo(string FullName, string Address1, string? Address2, string City, string Region, string PostalCode, string Country);
+    public readonly record struct MailingInfo(string FullName, string Address1, string? Address2, string City, string Region, string PostalCode, string Country);
 }
