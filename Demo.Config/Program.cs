cb3f4d01fa5e5ba9adf78a9e7c091991d339251f
Mel Grubb <MelGrubb@live.com>
2023-02-11 07:28:31 -0800

    0803-options

---
+++ Demo.Config/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Demo.Config;
+
+internal sealed class Program
+{
+    static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+        Console.WriteLine($"StringSetting = '{configuration.GetValue<string>("SampleSettings:StringSetting")}'");
+        Console.WriteLine($"IntSetting = {configuration.GetValue<int>("SampleSettings:IntSetting")}");
+        Console.WriteLine($"BoolSetting = {configuration.GetValue<bool>("SampleSettings:BoolSetting")}");
+        Console.WriteLine($"DateSetting = {configuration.GetValue<DateTime>("SampleSettings:DateSetting")}");
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
